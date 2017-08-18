44a664196082f49a07e85545c9b16904d28c1eab
Rodrigo Luiz Barbosa <rolubar@hotmail.com>
2017-08-18 12:59:46 -0300

    Update Create Trigger Example.sql
    Remove description invalid.

--- Create Trigger Example.sql
+++ Create Trigger Example.sql
@@ -5,7 +5,7 @@ MySQL 5.5.42
 Model of create trigger.
 */
 DELIMITER //
-CREATE DEFINER = `root`@`localhost` TRIGGER trg_name_before_insert_promotions
+CREATE DEFINER = `root`@`localhost` TRIGGER trg_name_before_insert
 BEFORE INSERT ON database_name.table_name
 FOR EACH ROW
 BEGIN
