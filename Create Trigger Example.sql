2790b55a05a61bf99981b199c81854e9c5ad3c3c
Rodrigo Luiz Barbosa <rolubar@hotmail.com>
2017-08-18 12:57:08 -0300

    Update Create Trigger Example.sql
    Alter header of script.

--- Create Trigger Example.sql
+++ Create Trigger Example.sql
@@ -1,3 +1,9 @@
+/*
+Rodrigo Luiz Barbosa
+24/10/2017
+MySQL 5.5.42
+Model of create trigger.
+*/
 DELIMITER //
 CREATE DEFINER = `root`@`localhost` TRIGGER trg_name_before_insert_promotions
 BEFORE INSERT ON database_name.table_name
